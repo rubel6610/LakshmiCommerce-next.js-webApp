3f6515b42df50acf7b56f8ea65d7647522411bc4
MD RUBEL HOSEN <rubelhosen1310@gmail.com>
2025-08-21 01:11:21 +0600

    added login form

---
+++ src/app/login/page.jsx
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react';
+import login from '@/app/Assets/lotties/login.json'
+import Lottie from "lottie-react";
+const Login = () => {
+    const handlesubmit = (e)=>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+    }
+    return (
+      <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className='w-full'>
+        <Lottie animationData={login} loop={true}/>
+        </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <form onSubmit={handlesubmit} className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" name='email' className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" name='password' className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Login;
