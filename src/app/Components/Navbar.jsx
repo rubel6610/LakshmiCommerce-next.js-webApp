4c33cf3044c09efe56af7e014e00a8292e6f14dc
MD RUBEL HOSEN <rubelhosen1310@gmail.com>
2025-08-21 00:43:26 +0600

    added navbar

---
+++ src/app/Components/Navbar.jsx
@@ -0,0 +1,43 @@
+
+import React from "react";
+import { FaBars } from 'react-icons/fa';
+
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <a href="">Home</a>
+      </li>
+      <li>
+        <a href="">Products</a>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <FaBars/>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">LakshmiCommerce</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
